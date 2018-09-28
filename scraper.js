8009bbbab7603a4f29d8526eba9912580633b66e
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-09-28 17:10:21 +0200

    obtained URLs for all shirts

--- scraper.js
+++ scraper.js
@@ -2,7 +2,8 @@ const cheerio = require('cheerio');
 const rp = require('request-promise');
 const fs = require('fs');
 
-
+const tShirts = [];
+const urls = [];
 
 //Program your scraper to check for a folder called ‘data’. 
 //If the folder doesn’t exist, the scraper should create one. If the folder does exist, the scraper should do nothing.
@@ -12,6 +13,28 @@ if(!fs.existsSync('./data')){
 }
 //Enter http://shirts4mike.com/shirts.php as single entry point to scrape information for 8 tee-shirts from the site,
 //
+
+const options = {
+	url: 'http://shirts4mike.com/shirts.php',
+	json: true,
+	transform: function(body){
+		return cheerio.load(body);
+	}
+}
+
+rp(options)
+	.then(($) => {
+		const $shirtLinks = $('.products').find('li').find('a');
+		$shirtLinks.each((index, element) => {
+			
+			urls[index] = $(element).attr('href');
+			console.log(urls[index]);
+		})
+
+	})
+	.catch(error => {
+		console.log(error);
+	})
 
 
 //The scraper should get the price, title, url and image url from the product page and save this information into a CSV file.
