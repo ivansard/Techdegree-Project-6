8e1463e9eeb7a3c60a93df3b622d3d165ded6550
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-09-28 15:56:20 +0200

    fs dependency added

---
+++ scraper.js
@@ -0,0 +1,14 @@
+const cheerio = require('cheerio');
+const rp = require('request-promise');
+const fs = require('fs');
+
+
+
+//Program your scraper to check for a folder called ‘data’. 
+//If the folder doesn’t exist, the scraper should create one. If the folder does exist, the scraper should do nothing.
+
+//Enter http://shirts4mike.com/shirts.php as single entry point to scrape information for 8 tee-shirts from the site,
+//
+
+
+//The scraper should get the price, title, url and image url from the product page and save this information into a CSV file.
