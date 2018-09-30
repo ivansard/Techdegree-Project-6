8d7f14112c20200fdf0977b10a7c7d9051b0365e
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-09-30 11:43:09 +0200

    writes data to csv file

--- scraper.js
+++ scraper.js
@@ -1,11 +1,17 @@
 const cheerio = require('cheerio');
 const rp = require('request-promise');
 const fs = require('fs');
-const csvWriterModule = require('csv-writer');
+const csv = require('fast-csv');
+const json2csv = require('json2csv');
 
 const tShirts = [];
 const urls = [];
 const today = new Date();
+
+const currentDate = `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`;
+
+const csvTableHeaders = ['Title', 'Price', 'Image URL', 'URL', 'Time of Entry'];
+const Json2csvParser = require('json2csv').Parser;
 
 //Program your scraper to check for a folder called ‘data’. 
 //If the folder doesn’t exist, the scraper should create one. If the folder does exist, the scraper should do nothing.
@@ -33,70 +39,78 @@ rp(options)
 			urls[index] = $(element).attr('href');
 			console.log(urls[index]);
 		});
-
-		// The scraper should get the price, title, url and image url from the product page and save this information into a CSV file.
-
-		for(let i = 0; i < urls.length; i++){
-			let optionsII = {
-				url: 'http://shirts4mike.com/' + urls[i],
-				json: true,
-				transform: function(body){
-					return cheerio.load(body);
-				}
-			}
-			rp(optionsII)
-				.then(($) => {
-					const shirtDetails = $('.shirt-details h1').text();
-					const price = shirtDetails.split(' ')[0].trim();
-
-					const breadcrumb = $('.breadcrumb').text();
-					const title = breadcrumb.split('>')[1].trim();
-
-					const imageUrl = $('.shirt-picture img').attr('src');
-					const pageUrl = optionsII.url;
-
-					const tShirt = {
-						title: title,
-						price: price,
-						imageUrl: imageUrl,
-						pageUrl: pageUrl
-						timeOfEntry: new Date().getTime();
-					};
-
-					tShirts.push(tShirt);
-
-				})
-				.catch((error) => {
-					console.log(error);
-				})
-		}
-
-			const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-			const csvWriter = createCsvWriter({
-			    path: `${today.getFullYear()}/${today.getMonth()}/${today.getDate()}.csv`,
-			    header: [
-			        {id: 'title', title: 'TITLE'},
-			        {id: 'price', title: 'PRICE'},
-			        {id: 'imageUrl', title: 'IMAGE URL'},
-			        {id: 'pageUrl', title: 'WEBPAGE URL'}
-			    ]
-			});
-
-			 
-			csvWriter.writeRecords(tShirts)       // returns a promise
-			    .then(() => {
-			        console.log('...Done');
-		    });
-			
-		
-
-
-
+		getShirtDetailsAndWriteToCsv();		
 	})
 	.catch(error => {
 		console.log(error);
 	})
 
 
-//The scraper should get the price, title, url and image url from the product page and save this information into a CSV file.
+function getShirtDetailsAndWriteToCsv(){
+		let i = 0;
+		function next(){
+			console.log(i);
+			if(i < urls.length){
+				let options = {
+					url : 'http://shirts4mike.com/' + urls[i],
+					json: true,
+					transform: function(body){
+						return cheerio.load(body);
+					}
+				}
+
+				rp(options)
+					.then(($) => {
+						const shirtDetails = $('.shirt-details h1').text();
+						const price = shirtDetails.split(' ')[0].trim();
+
+						const breadcrumb = $('.breadcrumb').text();
+						const title = breadcrumb.split('>')[1].trim();
+
+						const imageUrl = $('.shirt-picture img').attr('src');
+						const pageUrl = options.url;
+
+						let tShirt = {
+							title: title,
+							price: price,
+							imageUrl: imageUrl,
+							pageUrl: pageUrl,
+							timeOfEntry: new Date().toString()
+						};
+
+						tShirts.push(tShirt);
+						i = i+1;
+						return next();
+					})
+					.catch(error => {
+						console.error(error.message);
+					})
+			} else {
+				writeToCsvFile(tShirts);
+			}
+		}
+		return next();
+		
+}			
+
+function writeToCsvFile(data){
+	console.log('Im here');
+	try {
+	  const parser = new Json2csvParser({csvTableHeaders});
+	  const csvData = parser.parse(data);
+	  
+	  fs.writeFileSync(`./data/n/${currentDate}.csv`, csvData);
+	  console.log('Successfuly written to file!');
+	} catch (err) {		
+	  logError(err);
+	}
+}
+
+function logError(error){
+	fs.appendFile('./scraper-error.log', '\r\n' + `[${today.toString()}] ${error.message} ` + '\return\n', function(err){
+		if(err) throw err;
+		console.log('\n' + `[${today.toString()}] ${error.message} ` + '\n');
+		console.log('Written to error log file');
+		});
+}
 
